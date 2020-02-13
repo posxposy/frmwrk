c874af945a86a58034398d85d65d0040d322469a
Dmitry Hryppa <dmitry.hryppa@gmail.com>
2020-02-14 01:19:23 +0200

    use glfw window instead of own solution

--- libsrc/frmwrk/App.hx
+++ libsrc/frmwrk/App.hx
@@ -10,8 +10,8 @@ import sys.io.File;
 		<compilerflag value="-I${haxelib:frmwrk}/bx/include/"/>
 		<compilerflag value="-I${haxelib:frmwrk}/bx/include/compat/msvc/"/>
 		<compilerflag value="-I${haxelib:frmwrk}/bimg/include/"/>
-
 		<compilerflag value="-I${haxelib:frmwrk}/glm/"/>
+		<compilerflag value="-I${haxelib:frmwrk}/glfw/include/"/>
 	</files>
 
 	<files id="__main__">
@@ -19,8 +19,8 @@ import sys.io.File;
 		<compilerflag value="-I${haxelib:frmwrk}/bx/include/"/>
 		<compilerflag value="-I${haxelib:frmwrk}/bx/include/compat/msvc/"/>
 		<compilerflag value="-I${haxelib:frmwrk}/bimg/include/"/>
-
 		<compilerflag value="-I${haxelib:frmwrk}/glm/"/>
+		<compilerflag value="-I${haxelib:frmwrk}/glfw/include/"/>
 	</files>
 
 	<target id="haxe" tool="linker" toolid="exe">
@@ -31,6 +31,7 @@ import sys.io.File;
 		<lib name="${haxelib:frmwrk}/bgfx/.build/win32_vs2019/bin/bxRelease.lib" />
 		<lib name="${haxelib:frmwrk}/bgfx/.build/win32_vs2019/bin/bimgRelease.lib" />
 		<lib name="${haxelib:frmwrk}/bgfx/.build/win32_vs2019/bin/bimg_decodeRelease.lib" />
+		<lib name="${haxelib:frmwrk}/glfw/lib/glfw3dll.lib" />
 	</target>
 ')
 @:headerCode('
@@ -41,75 +42,17 @@ import sys.io.File;
 	#include <bx/math.h>
 	#include <bgfx/bgfx.h>
 	#include <bgfx/platform.h>
+
+	#include <GLFW/glfw3.h>
+#if BX_PLATFORM_LINUX
+	#define GLFW_EXPOSE_NATIVE_X11
+#elif BX_PLATFORM_WINDOWS
+	#define GLFW_EXPOSE_NATIVE_WIN32
+#endif
+	#include <GLFW/glfw3native.h>
 ')
 @:headerClassCode('
-	HWND m_handle;
-	MSG msg;
-')
-@:cppFileCode('
-LRESULT CALLBACK winProc (HWND handle, UINT msg, WPARAM wparam, LPARAM lparam) {
-	if (msg == WM_DESTROY || msg == WM_CLOSE) {
-			PostQuitMessage(0);
-			return 0;
-	}
-
-	switch (msg) {
-			case WM_KEYDOWN:
-					if (wparam == VK_ESCAPE) {
-							if (MessageBox(nullptr, "Are you sure you want to exit?", "Really?", MB_YESNO | MB_ICONQUESTION) == IDYES) {
-									//Release the windows allocated memory
-									DestroyWindow(handle);
-							}
-					}
-					return 0;
-
-			case WM_DESTROY:	//if x button in top right was pressed
-					PostQuitMessage(0);
-					return 0;
-
-			case WM_SYSKEYDOWN:
-			case WM_KEYUP:
-			case WM_SYSKEYUP:
-					//DirectX::Keyboard::ProcessMessage(msg, wparam, lparam);
-					break;
-			case WM_ACTIVATEAPP:
-					//DirectX::Keyboard::ProcessMessage(msg, wparam, lparam);
-					//DirectX::Mouse::ProcessMessage(msg, wparam, lparam);
-					break;
-			case WM_INPUT:
-			case WM_MOUSEMOVE:
-			case WM_LBUTTONDOWN:
-			case WM_LBUTTONUP:
-			case WM_RBUTTONDOWN:
-			case WM_RBUTTONUP:
-			case WM_MBUTTONDOWN:
-			case WM_MBUTTONUP:
-			case WM_MOUSEWHEEL:
-			case WM_XBUTTONDOWN:
-			case WM_XBUTTONUP:
-			case WM_MOUSEHOVER:
-					//DirectX::Mouse::ProcessMessage(msg, wparam, lparam);
-					break;
-	}
-
-	return DefWindowProc(handle, msg, wparam, lparam);
-}
-
-bgfx::ShaderHandle loadShader(const char *FILENAME)
-{
-
-    FILE *file = fopen(FILENAME, "rb");
-    fseek(file, 0, SEEK_END);
-    long fileSize = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    const bgfx::Memory *mem = bgfx::alloc(fileSize + 1);
-    fread(mem->data, 1, fileSize, file);
-    //mem->data[mem->size - 1] = 0;
-    fclose(file);
-
-    return bgfx::createShader(mem);
-}
+	GLFWwindow *window = nullptr;
 ')
 @:headerInclude('./HelpersExt.h')
 final class App {
@@ -117,6 +60,9 @@ final class App {
 	public var width(default, null):Int;
 	public var height(default, null):Int;
 	public var renderer(default, null):RendererType;
+
+	extern var window:GLFWwindow;
+	var isVsyncEnabled:Bool;
 
 	final gfx:Gfx;
 	final gameTime:GameTime;
@@ -130,46 +76,36 @@ final class App {
 		this.width = width;
 		this.height = height;
 		this.renderer = renderer;
+		this.isVsyncEnabled = isVsyncEnabled;
 
 		untyped __cpp__('
-			std::memset(&msg, 0, sizeof(msg));
+			if (!glfwInit()) {
+				return false;
+			}
+			glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+			window = glfwCreateWindow({1}, {2}, "helloworld", nullptr, nullptr);
+			if (!window) {
+				return false;
+			}
 
-			WNDCLASS wc;
-			std::memset(&wc, 0, sizeof(wc));
-			wc.style = CS_OWNDC;
-			wc.lpfnWndProc = winProc;
-			wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-			wc.lpszClassName = "frmwrk";
-			RegisterClass(&wc);
-
-			auto screenWidth = GetSystemMetrics(SM_CXSCREEN);
-			auto screenHeight = GetSystemMetrics(SM_CYSCREEN);
-
-			DWORD style = WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_VISIBLE;
-			RECT wnd = {0, 0, {1}, {2}};
-			AdjustWindowRect(&wnd, style, FALSE);
-
-			int w = (wnd.right - wnd.left);
-			int h = (wnd.bottom - wnd.top);
-
-			m_handle = CreateWindow (
-				"frmwrk",
-				{0}.c_str(),
-				style,
-				(screenWidth - {1}) / 2, (screenHeight - {2}) / 2,
-				w,
-				h,
-				nullptr,
-				nullptr,
-				nullptr,
-				nullptr
-			);
-
+			bgfx::renderFrame();
 			bgfx::Init init;
 			init.type = {5};
 			init.resolution.width = {1};
 			init.resolution.height = {2};
-			init.platformData.nwh = m_handle;
+
+			int _width, _height;
+			glfwGetWindowSize(window, &_width, &_height);
+
+			#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+				init.platformData.ndt = glfwGetX11Display();
+				init.platformData.nwh = (void*)(uintptr_t)glfwGetX11Window(window);
+			#elif BX_PLATFORM_OSX
+				init.platformData.nwh = glfwGetCocoaWindow(window);
+			#elif BX_PLATFORM_WINDOWS
+				init.platformData.nwh = glfwGetWin32Window(window);
+			#endif
+
 			if ({3}){ //! Not supported yet by BGFX
 				init.resolution.reset |= BGFX_RESET_FULLSCREEN;
 			}
@@ -193,41 +129,15 @@ final class App {
 		', @:privateAccess gfx.viewId);
 
 		gfx.clearColor(0x443355FF);
-
-		final counter:Int = 0;
-		while (true) {
+		while (!glfwWindowShouldClose(window)) {
 			gameTime.begin();
 
 			untyped __cpp__('
-				if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
-					if (msg.message == WM_QUIT) {
-						break;
-					}
-					TranslateMessage(&msg);
-					DispatchMessage(&msg);
-				}
-
+				glfwPollEvents();
 				bgfx::touch(clearViewId);
-			');
+			', width, height, window, isVsyncEnabled);
 
 			game.onUpdate(gameTime);
-
-			untyped __cpp__('
-				bgfx::dbgTextClear();
-				counter++;
-				/*const bx::Vec3 at = {0.0f, 0.0f,  0.0f};
-				const bx::Vec3 eye = {0.0f, 0.0f, -5.0f};
-				float view[16];
-				bx::mtxLookAt(view, eye, at);
-				float proj[16];
-				bx::mtxProj(proj, 60.0f, 1.33f, 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
-				bgfx::setViewTransform(0, view, proj);
-
-				float mtx[16];
-				bx::mtxRotateXY(mtx, counter * 0.01f, counter * 0.01f);
-				bgfx::setTransform(mtx);*/
-			');
-
 			game.onDraw(gfx);
 
 			untyped __cpp__('
@@ -238,11 +148,18 @@ final class App {
 			gameTime.end();
 		}
 
-		untyped __cpp__('bgfx::shutdown()');
+		untyped __cpp__('
+			bgfx::shutdown();
+			glfwTerminate()
+		');
 	}
 
 	public inline extern function bytesToMemory(bytes:Bytes):BgfxMemory {
 		return untyped __cpp__('Helpers::getMemory( (uint8_t *const)&({0}[0]), {1} )', bytes.getData(), bytes.length);
+	}
+
+	public inline extern function glfwWindowShouldClose(window:GLFWwindow):Bool {
+		return untyped __cpp__('glfwWindowShouldClose({0})', window);
 	}
 }
 
@@ -250,4 +167,10 @@ final class App {
 @:native('const bgfx::Memory *')
 @:include('bgfx/bgfx.h')
 extern class BgfxMemory {
+}
+
+@:unreflective
+@:native('GLFWwindow *')
+@:include('GLFW/glfw3.h')
+extern class GLFWwindow {
 }
